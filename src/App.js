c58f4d5a5bd311be11aeafb67c272cc2839faec1
leedooseung <leedoseu@gmail.com>
2019-12-04 16:52:59 +0900

    최적화 - react-virtualized

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useReducer, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
@@ -6,7 +6,7 @@ import TodoList from './components/TodoList';
 function createBulkTodos() {
   const array = [];
 
-  for(let i = 1; i <= 2500; i++){
+  for (let i = 1; i <= 2500; i++) {
     array.push({
       id: i,
       text: `할 일 ${i}`,
@@ -15,38 +15,56 @@ function createBulkTodos() {
   }
   return array;
 }
+
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case 'INSERT':
+      return todos.concat(action.todo);
+    case 'REMOVE':
+      return todos.filter(todo => todo.id !== action.id);
+    case 'TOGGLE':
+      return todos.map(todo =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+      );
+    default:
+      return todos;
+  }
+}
+
 const App = () => {
-  const [todos, setTodos] = useState(createBulkTodos);
+  // useState 사용법
+  //const [todos, setTodos] = useState(createBulkTodos);
+
+  // useReducer 사용법
+  // useReducer 2번째 파라미터에 undefined를 넣고, 세번째 파라미터에 초기 상태를 넣어주면 컴포넌트가 맨 처음 렌더링 될 때만 호출된다.
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
 
   const nextId = useRef(2501);
 
-  const onInsert = useCallback(
-    text => {
-      const todo = {
-        id: nextId.current,
-        text,
-        checked: false,
-      };
-      setTodos(todos => todos.concat(todo));
-      nextId.current += 1;
-    },
-    []);
+  const onInsert = useCallback(text => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    //setTodos(todos => todos.concat(todo));
+    dispatch({ type: 'INSERT', todo });
+    nextId.current += 1;
+  }, []);
 
-  const onRemove = useCallback(
-    id => {
-      setTodos(todos => todos.filter(todo => todo.id !== id));
-    },
-    []);
+  const onRemove = useCallback(id => {
+    //setTodos(todos => todos.filter(todo => todo.id !== id));
+    dispatch({ type: 'REMOVE', id });
+  }, []);
 
-  const onToggle = useCallback(
-    id => {
-      setTodos( todos =>
-        todos.map(todo =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-        ),
-      );
-    },
-    []);
+  const onToggle = useCallback(id => {
+    /* setTodos(todos =>
+      todos.map(todo =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+      ),
+    ); */
+    dispatch({ type: 'TOGGLE', id });
+  }, []);
 
   return (
     <TodoTemplate>
